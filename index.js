c0dd90079d68d81f350918580db7af986769d3a2
eric-chambonneau <eric.chambonneau@gmail.com>
2022-05-08 08:05:54 +0200

    3init

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-// alert("le fichier fonctionne !");
-console.log("hello");
+let chiffre = 47
+
+let nouvelleChaine = `Le chiffre attendu est : ${chiffre} degrés`;
+
+console.log(nouvelleChaine);
+
+let string = "je suis une chaine de caractère";
+let number = 24;
+let boolean = true;
+let array = ["je", "suis", 24, false];
+
+let object = {
+    prenom: "audrey",
+    age: 34,
+    ville: "Bordeaux",
+};
+
+console.log(typeof array);
+console.log(typeof number);
+
+
+
+
